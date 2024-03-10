8bd427dd501caf6b0baf63e237b952f51d3e1481
Wei Sun <sunwei415@126.com>
2024-03-11 05:23:39 +0800

    wip replacing bufferknife

--- app/src/main/java/sq/rogue/rosettadrone/fragment/actuator/AircraftActuatorFragment.java
+++ app/src/main/java/sq/rogue/rosettadrone/fragment/actuator/AircraftActuatorFragment.java
@@ -4,24 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-
-import androidx.appcompat.widget.AppCompatCheckBox;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
 import dji.common.mission.waypointv2.Action.ActionTypes;
 import dji.common.mission.waypointv2.Action.WaypointActuator;
 import dji.common.mission.waypointv2.Action.WaypointAircraftControlParam;
 import dji.common.mission.waypointv2.Action.WaypointAircraftControlRotateYawParam;
 import dji.common.mission.waypointv2.Action.WaypointAircraftControlStartStopFlyParam;
 import dji.common.mission.waypointv2.WaypointV2MissionTypes;
-import sq.rogue.rosettadrone.R;
 import sq.rogue.rosettadrone.settings.Tools;
+import wiley.sq.rogue.rosettadrone.R;
+import wiley.sq.rogue.rosettadrone.databinding.FragmentAircraftActuatorBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,27 +23,8 @@ import sq.rogue.rosettadrone.settings.Tools;
 public class AircraftActuatorFragment extends Fragment implements IActuatorCallback {
 
 
-    Unbinder unbinder;
-    @BindView(R.id.rb_rotate_yaw)
-    RadioButton rbRotateYaw;
-    @BindView(R.id.rb_start_stop_fly)
-    RadioButton rbStartStopFly;
-    @BindView(R.id.radio_type)
-    RadioGroup radioType;
-    @BindView(R.id.tv_tip_yaw)
-    TextView tvTipYaw;
-    @BindView(R.id.box_yaw_relative)
-    AppCompatCheckBox boxYawRelative;
-    @BindView(R.id.box_yaw_clockwise)
-    AppCompatCheckBox boxYawClockwise;
-    @BindView(R.id.cl_yaw)
-    ConstraintLayout clYaw;
-    @BindView(R.id.box_start_stop_fly)
-    AppCompatCheckBox boxStartStopFly;
-    @BindView(R.id.cl_start_stop)
-    ConstraintLayout clStartStop;
-    @BindView(R.id.et_yaw_angle)
-    TextView yawAngle;
+    private FragmentAircraftActuatorBinding binding;
+
 
     private ActionTypes.AircraftControlType type = ActionTypes.AircraftControlType.UNKNOWN;
 
@@ -62,41 +35,44 @@ public class AircraftActuatorFragment extends Fragment implements IActuatorCallb
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_aircraft_actuator, container, false);
-        unbinder = ButterKnife.bind(this, root);
-        radioType.setOnCheckedChangeListener((group, checkedId) -> {
+        binding = FragmentAircraftActuatorBinding.inflate(inflater, container, false);
+        setupRadioGroup();
+        return binding.getRoot();
+    }
+
+    private void setupRadioGroup() {
+        binding.radioType.setOnCheckedChangeListener((group, checkedId) -> {
             switch (checkedId) {
                 case R.id.rb_rotate_yaw:
-                    clYaw.setVisibility(View.VISIBLE);
-                    clStartStop.setVisibility(View.GONE);
+                    binding.clYaw.setVisibility(View.VISIBLE);
+                    binding.clStartStop.setVisibility(View.GONE);
                     type = ActionTypes.AircraftControlType.ROTATE_YAW;
                     break;
                 case R.id.rb_start_stop_fly:
-                    clStartStop.setVisibility(View.VISIBLE);
-                    clYaw.setVisibility(View.GONE);
+                    binding.clStartStop.setVisibility(View.VISIBLE);
+                    binding.clYaw.setVisibility(View.GONE);
                     type = ActionTypes.AircraftControlType.START_STOP_FLY;
                     break;
             }
         });
-        return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        unbinder.unbind();
+        binding = null;
     }
 
     @Override
     public WaypointActuator getActuator() {
-        float yaw = Tools.getFloat(yawAngle.getText().toString(), 0);
+        float yaw = Tools.getFloat(binding.etYawAngle.getText().toString(), 0);
         WaypointAircraftControlRotateYawParam yawParam = new WaypointAircraftControlRotateYawParam.Builder()
-                .setDirection(boxYawClockwise.isChecked() ? WaypointV2MissionTypes.WaypointV2TurnMode.CLOCKWISE : WaypointV2MissionTypes.WaypointV2TurnMode.COUNTER_CLOCKWISE)
-                .setRelative(boxYawRelative.isChecked())
+                .setDirection(binding.boxYawClockwise.isChecked() ? WaypointV2MissionTypes.WaypointV2TurnMode.CLOCKWISE : WaypointV2MissionTypes.WaypointV2TurnMode.COUNTER_CLOCKWISE)
+                .setRelative(binding.boxYawRelative.isChecked())
                 .setYawAngle(yaw)
                 .build();
         WaypointAircraftControlStartStopFlyParam startParam = new WaypointAircraftControlStartStopFlyParam.Builder()
-                .setStartFly(boxStartStopFly.isChecked())
+                .setStartFly(binding.boxStartStopFly.isChecked())
                 .build();
         WaypointAircraftControlParam controlParam = new WaypointAircraftControlParam.Builder()
                 .setAircraftControlType(type)
